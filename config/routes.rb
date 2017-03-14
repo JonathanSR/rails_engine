6b959b902d0407bfa0f209ad704df7d6c6914a01
Jonathan Serrano <jonathan.serranor@gmail.com>
2017-03-13 21:22:39 -0600

    adds random controller and method for invoices creates tests for finding all queries, create and find by testing broken

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
       namespace :invoices do
         get '/find', to: 'search_invoices#show'
+        get'/find_all', to: 'search_invoices#index'
+        get'/random', to: 'random_invoices#show'
       end
       resources :invoices, only: [:index, :show]
     end
--- spec/requests/api/v1/invoices_spec.rb
+++ spec/requests/api/v1/invoices_spec.rb
@@ -49,5 +49,118 @@ describe "Invoices API" do
     expect(response).to be_success
     expect(returned_invoice["merchant_id"]).to eq(invoice.merchant_id)
   end
+
+  it "returns a single invoice by status" do
+    invoice = create(:invoice)
+
+    get "/api/v1/invoices/find?status=#{invoice.status}"
+
+    returned_invoice = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(returned_invoice["status"]).to eq(invoice.status)
+  end
+    
+  xit "returns a single invoice by created_at" do
+    invoice = create(:invoice)
+#byebug
+    get "/api/v1/invoices/find?created_at=#{invoice.created_at}"
+
+    returned_invoice = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(returned_invoice["created_at"]).to eq(invoice.created_at)
+  end
+
+  xit "returns a single invoice by updated_at" do
+    invoice = create(:invoice)
+
+    get "/api/v1/invoices/find?updated_at=#{invoice.updated_at}"
+
+    returned_invoice = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(returned_invoice["created_at"]).to eq(invoice.updated_at)
+  end
+
+it "returns all invoices with same customer_id" do
+    create_list(:invoice, 3)
+    customer = create(:customer)
+    create_list(:invoice, 3, customer: customer)
+
+    get "/api/v1/invoices/find_all?customer_id=#{customer.id}"
+
+    returned_invoices = JSON.parse(response.body)
+    first_invoice = returned_invoices.first
+
+
+    expect(response).to be_success
+    expect(returned_invoices.count).to eq(3)
+    expect(first_invoice["customer_id"]).to eq(customer.id)
+  end
+
+it "returns all invoices with same merchant_id" do
+    create_list(:invoice, 3)
+    merchant = create(:merchant)
+    create_list(:invoice, 3, merchant: merchant)
+
+    get "/api/v1/invoices/find_all?merchant_id=#{merchant.id}"
+
+    returned_invoices = JSON.parse(response.body)
+    first_invoice = returned_invoices.first
+
+
+    expect(response).to be_success
+    expect(returned_invoices.count).to eq(3)
+    expect(first_invoice["merchant_id"]).to eq(merchant.id)
+  end
+
+  it "returns all invoices with same status" do
+    create_list(:invoice, 3)
+    create_list(:invoice, 3, status: "ordered")
+
+    get "/api/v1/invoices/find_all?status=ordered"
+
+    returned_invoices = JSON.parse(response.body)
+    first_invoice = returned_invoices.first
+
+
+    expect(response).to be_success
+    expect(returned_invoices.count).to eq(3)
+    expect(first_invoice["status"]).to eq("ordered")
+  end
+
+  xit "returns all invoices with same created_at" do
+    create_list(:invoice, 3)
+
+    get "/api/v1/invoices/find_all?created_at=#{Invoice.first.created_at}"
+
+    returned_invoices = JSON.parse(response.body)
+    first_invoice = returned_invoices.first
+
+    expect(response).to be_success
+    expect(returned_invoices.count).to eq(3)
+  end
+  
+  xit "returns all invoices with same updated_at" do
+    create_list(:invoice, 3)
+
+    get "/api/v1/invoices/find_all?updated_at=#{Invoice.first.updated_at}"
+
+    returned_invoices = JSON.parse(response.body)
+    first_invoice = returned_invoices.first
+
+    expect(response).to be_success
+    expect(returned_invoices.count).to eq(3)
+  end
+
+  it "returns a random invoice record" do
+    create_list(:invoice, 3)
+    get '/api/v1/invoices/random'
+
+    invoice = JSON.parse(response.body)
+
+    expect(response).to be_success
+  end
 end
 
