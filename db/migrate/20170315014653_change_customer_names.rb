64932c41177655accf702e7c03e2447ea6763b72
Jonathan Serrano <jonathan.serranor@gmail.com>
2017-03-14 19:54:38 -0600

    changes all text attributes to citext

---
+++ db/migrate/20170315014653_change_customer_names.rb
@@ -0,0 +1,7 @@
+class ChangeCustomerNames < ActiveRecord::Migration[5.0]
+  def change
+    enable_extension :citext
+    change_column :customers, :first_name, :citext
+    change_column :customers, :last_name, :cite
+  end
+end
